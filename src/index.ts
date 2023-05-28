f81a756f2d8fb630fc9297e49d8a6287f450a726
Shayan-P <shayanpardis82@gmail.com>
2023-05-27 22:34:49 -0400

    adjust the speed

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const svg = select('body')
 
 const vis = new GraphVisualizer(svg.node() ?? assert.fail());
 
-const averageDelayBetween = 10;
+const averageDelayBetween = 200;
 
 let counter = 0;
 const map = new Map<Tree, Node>();
